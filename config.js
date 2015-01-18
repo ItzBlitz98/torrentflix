0a9c99bffbcf3f6a1ee117908454739bb15b7c38
itzblitz98 <gfxscene@gmail.com>
2015-01-18 00:32:07 +0000

    adding subtitle support

--- config.js
+++ config.js
@@ -10,13 +10,20 @@ var peerflix_player = "--mpv";
 var peerflix_player_args = "--not-on-top";
 /* what port you want peerflix to use */
 var peerflix_port = "--port=8888";
-
+/* command to run peerflix (most of the time you can leave this) */
 var peerflix_command = "peerflix";
+/* do you want to fetch subtitles ? (true/false) */
+var use_subtitle = "true";
+/* subtitle language */
+/* please use the language abbriviation */
+/* EX: en = English or es = Spanish */
+var subtitle_language = "eng";
+
+
 
 /*leave everything below */
 var data_content = {};
 var config_content = [];
-
 
 module.exports = {
   getConfig: function() {
@@ -27,7 +34,9 @@ module.exports = {
       peerflix_player: peerflix_player,
       peerflix_player_args: peerflix_player_args,
       peerflix_port: peerflix_port,
-      peerflix_command: peerflix_command
+      peerflix_command: peerflix_command,
+      use_subtitle: use_subtitle,
+      subtitle_language: subtitle_language
     };
 
     config_content.push(data_content);
